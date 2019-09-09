8a615fe728f99565612117a8620c86506f03d155
Karmo Kuurberg <karmo.kuurberg@cdptech.com>
2019-09-09 09:53:07 +0300

    Clean indentation

--- src/main/java/com/cdptech/cdpclient/Client.java
+++ src/main/java/com/cdptech/cdpclient/Client.java
@@ -36,7 +36,7 @@ client.run();
  * </pre>
  */
 public class Client implements Runnable {
-  
+
   private Map<URI, RequestDispatch> connections;
   private Set<URI> lostConnections = new HashSet<>();
   private Node rootNode;
@@ -50,181 +50,181 @@ public class Client implements Runnable {
   private boolean clientClosed = false;
 
   /** Create a new StudioAPI Client instance. */
-	public Client() {
-		connections = new HashMap<>();
-		rootNode = null;
-		listener = null;
-		cleanupConnections = false;
-		timeSyncEnabled = true;
-	}
+  public Client() {
+      connections = new HashMap<>();
+      rootNode = null;
+      listener = null;
+      cleanupConnections = false;
+      timeSyncEnabled = true;
+  }
 
-	/** Initialise the client. Connects to server and notifies @a listener. */
-	public void init(String addr, int port, NotificationListener listener) {
-	  this.listener = listener;
-	  clientClosed = false;
-    try {
-      URI wsURI = new URI("ws", null, addr, port, null, null, null);
-      if (connections.containsKey(wsURI))
-      	return;
-      ioHandler = new IOHandler(wsURI);
+  /** Initialise the client. Connects to server and notifies @a listener. */
+  public void init(String addr, int port, NotificationListener listener) {
+    this.listener = listener;
+    clientClosed = false;
+  try {
+    URI wsURI = new URI("ws", null, addr, port, null, null, null);
+    if (connections.containsKey(wsURI))
+      return;
+    ioHandler = new IOHandler(wsURI);
+    ioHandler.setTimeSyncEnabled(timeSyncEnabled);
+    RequestDispatch d = new RequestDispatch(this, ioHandler);
+    d.init();
+    connections.put(wsURI, d);
+  } catch (URISyntaxException e) {
+    throw new IllegalArgumentException("Unable to parse server URI");
+  }
+  }
+
+  /** Get a reference to the system node. CDP applications are children of this node. */
+  public Node getRootNode() {
+    return rootNode;
+  }
+
+  /**
+    * (asynchronous) Request node with provided path.
+    * @param nodePath Should contain dot separated path to target node.
+    */
+  public Request findNode(String nodePath) {
+      return getRootNode().find(nodePath);
+  }
+
+  /**
+    * Sets whether to enable automatic and periodic time sync. When enabled, the timestamps
+    * received from remote machines (e.g. values received after calling subscribeToValueChanges())
+    * are adjusted for the clock difference between this machine and remote machine.
+    */
+  public void setTimeSyncEnabled(boolean enabled) {
+    timeSyncEnabled = enabled;
+    if (ioHandler != null)
       ioHandler.setTimeSyncEnabled(timeSyncEnabled);
-      RequestDispatch d = new RequestDispatch(this, ioHandler);
-      d.init();
-      connections.put(wsURI, d);
-    } catch (URISyntaxException e) {
-      throw new IllegalArgumentException("Unable to parse server URI");
+  }
+
+  public void setAutoReconnect(boolean enabled) {
+      this.autoReconnect = enabled;
+  }
+
+  /** Event-loop method for use in single-threaded applications. */
+  public void process() {
+    for (RequestDispatch dispatch : connections.values())
+        dispatch.service();
+    if (cleanupConnections)
+      removeDroppedConnections();
+    if (autoReconnect && !clientClosed  && (System.currentTimeMillis() - lastReconnectTimeMs > 200)) {
+      for (URI uri : lostConnections)
+        init(uri.getHost(), uri.getPort(), this.listener);
+      lastReconnectTimeMs = System.currentTimeMillis();
     }
-	}
+  }
 
-	/** Get a reference to the system node. CDP applications are children of this node. */
-	public Node getRootNode() {
-	  return rootNode;
-	}
+  /** Runnable-interface auto-creates event loop in a new Thread. */
+  public void run() {
+  while (true) {
+    process();
+    if (clientClosed)
+      break;
+    try {
+      Thread.sleep(10);
+    } catch (InterruptedException e) {
+      e.printStackTrace();
+    }
+  }
+  }
 
-	/**
-	 * (asynchronous) Request node with provided path.
-	 * @param nodePath Should contain dot separated path to target node.
-	 */
-	public Request findNode(String nodePath) {
-		return getRootNode().find(nodePath);
-	}
+  /** Closes all connection and disables automatic reconnect. */
+  public void close() {
+    clientClosed = true;
+    for (RequestDispatch dispatch : connections.values())
+      dispatch.close();
+  }
 
-	/**
-	 * Sets whether to enable automatic and periodic time sync. When enabled, the timestamps
-	 * received from remote machines (e.g. values received after calling subscribeToValueChanges())
-	 * are adjusted for the clock difference between this machine and remote machine.
-	 */
-	public void setTimeSyncEnabled(boolean enabled) {
-	  timeSyncEnabled = enabled;
-	  if (ioHandler != null)
-	    ioHandler.setTimeSyncEnabled(timeSyncEnabled);
-	}
+  /** Called internally to set the root node of the system. */
+  void setRootNode(Node node) {
+    rootNode = node;
+  }
 
-	public void setAutoReconnect(boolean enabled) {
-		this.autoReconnect = enabled;
-	}
-
-	/** Event-loop method for use in single-threaded applications. */
-	public void process() {
-	  for (RequestDispatch dispatch : connections.values())
-		  dispatch.service();
-	  if (cleanupConnections)
-	    removeDroppedConnections();
-	  if (autoReconnect && !clientClosed  && (System.currentTimeMillis() - lastReconnectTimeMs > 200)) {
-	    for (URI uri : lostConnections)
-	      init(uri.getHost(), uri.getPort(), this.listener);
-	    lastReconnectTimeMs = System.currentTimeMillis();
-	  }
-	}
-
-	/** Runnable-interface auto-creates event loop in a new Thread. */
-	public void run() {
-    while (true) {
-      process();
-      if (clientClosed)
+  /** Called by a dispatch to notify it's ready. */
+  void dispatchReady(RequestDispatch dispatch) {
+    for (RequestDispatch d : connections.values()) {
+      if (d.getState() != RequestDispatch.State.ESTABLISHED)
         break;
-      try {
-        Thread.sleep(10);
-      } catch (InterruptedException e) {
-        e.printStackTrace();
-      }
     }
-	}
+  listener.clientReady(this);
+  }
 
-	/** Closes all connection and disables automatic reconnect. */
-	public void close() {
-	  clientClosed = true;
-	  for (RequestDispatch dispatch : connections.values())
-	    dispatch.close();
-	}
-
-	/** Called internally to set the root node of the system. */
-	void setRootNode(Node node) {
-	  rootNode = node;
-	}
-
-	/** Called by a dispatch to notify it's ready. */
-	void dispatchReady(RequestDispatch dispatch) {
-	  for (RequestDispatch d : connections.values()) {
-	    if (d.getState() != RequestDispatch.State.ESTABLISHED)
-	      break;
-	  }
-    listener.clientReady(this);
-	}
-
-	/** Called by a dispatch to notify it's connection was lost. */
-	void dispatchDropped(RequestDispatch dispatch) {
-	  // notify all involved nodes if they have listeners
-	  if (rootNode != null) {
-	    for (int i = 0; i < rootNode.getChildCount(); i++) {
-	  	  Node n = rootNode.getCachedChild(i);
-		  if (n.getDispatch() == dispatch) {
-		  	n.notifyNodeIsLost();
-		  }
-	    }
-	  }
-
-	  cleanupConnections = true;
-	}
-
-	/** Open connection to unconnected top-level node. */
-    void openConnection(Node app) {
-      if (app.getDispatch() == null) {
-        Node.ConnectionData data = app.getConnectionData();
-        try {
-          URI wsURI = new URI("ws", null,
-                  data.serverAddr, data.serverPort,
-                  null, null, null);
-          if (connections.containsKey(wsURI))
-          	return;
-          IOHandler io = new IOHandler(wsURI);
-          ioHandler.setTimeSyncEnabled(timeSyncEnabled);
-          RequestDispatch d = new RequestDispatch(this, io);
-          d.init(app);
-          connections.put(wsURI, d);
-        } catch (URISyntaxException e) {
-          cleanupConnections = true;
+  /** Called by a dispatch to notify it's connection was lost. */
+  void dispatchDropped(RequestDispatch dispatch) {
+    // notify all involved nodes if they have listeners
+    if (rootNode != null) {
+      for (int i = 0; i < rootNode.getChildCount(); i++) {
+        Node n = rootNode.getCachedChild(i);
+        if (n.getDispatch() == dispatch) {
+          n.notifyNodeIsLost();
         }
       }
     }
 
-	/** Post-process cleanup method. */
-	private void removeDroppedConnections() {
-	  // remove dispatches
-    Iterator<Map.Entry<URI, RequestDispatch>> it = connections.entrySet().iterator();
-    while (it.hasNext()) {
-	  Map.Entry<URI, RequestDispatch> entry = it.next();
-      if (entry.getValue().getState() != RequestDispatch.State.ESTABLISHED) {
-      	lostConnections.add(entry.getKey());
-        it.remove();
+    cleanupConnections = true;
+  }
+
+  /** Open connection to unconnected top-level node. */
+  void openConnection(Node app) {
+    if (app.getDispatch() == null) {
+      Node.ConnectionData data = app.getConnectionData();
+      try {
+        URI wsURI = new URI("ws", null,
+                data.serverAddr, data.serverPort,
+                null, null, null);
+        if (connections.containsKey(wsURI))
+          return;
+        IOHandler io = new IOHandler(wsURI);
+        ioHandler.setTimeSyncEnabled(timeSyncEnabled);
+        RequestDispatch d = new RequestDispatch(this, io);
+        d.init(app);
+        connections.put(wsURI, d);
+      } catch (URISyntaxException e) {
+        cleanupConnections = true;
       }
     }
+  }
 
-	  // remove nodes left without dispatches
-	  if (rootNode != null) {
-	    ListIterator<Node> iter = rootNode.getChildList().listIterator();
-	    while (iter.hasNext()) {
-		  Node n = iter.next();
-		  if (n.getDispatch() == null || !connections.containsValue(n.getDispatch())) {
-		    lostApps.add(n);
-	  	    n.setParent(null);
-			iter.remove();
-		  }
-		}
-	  }
-	  cleanupConnections = false;
-	  if (connections.isEmpty()) {
-	    if (!autoReconnect) {
-	      clientClosed = true;
-	      listener.clientClosed(this);
-	    }
-        return;
+  /** Post-process cleanup method. */
+  private void removeDroppedConnections() {
+    // remove dispatches
+  Iterator<Map.Entry<URI, RequestDispatch>> it = connections.entrySet().iterator();
+  while (it.hasNext()) {
+    Map.Entry<URI, RequestDispatch> entry = it.next();
+    if (entry.getValue().getState() != RequestDispatch.State.ESTABLISHED) {
+      lostConnections.add(entry.getKey());
+      it.remove();
+    }
+  }
+
+    // remove nodes left without dispatches
+    if (rootNode != null) {
+      ListIterator<Node> iter = rootNode.getChildList().listIterator();
+      while (iter.hasNext()) {
+        Node n = iter.next();
+        if (n.getDispatch() == null || !connections.containsValue(n.getDispatch())) {
+          lostApps.add(n);
+          n.setParent(null);
+          iter.remove();
+        }
       }
-	  if (rootNode != null && !connections.containsValue(rootNode.getDispatch()))
-	    rootNode.setDispatch(connections.values().iterator().next());
-	}
+    }
+    cleanupConnections = false;
+    if (connections.isEmpty()) {
+      if (!autoReconnect) {
+        clientClosed = true;
+        listener.clientClosed(this);
+      }
+      return;
+    }
+    if (rootNode != null && !connections.containsValue(rootNode.getDispatch()))
+      rootNode.setDispatch(connections.values().iterator().next());
+  }
 
-	/** Broadcast a root structure subscription to everyone but its handler. */
+  /** Broadcast a root structure subscription to everyone but its handler. */
   void broadcastStructureSubscription() {
     for (RequestDispatch d : connections.values()) {
       if (rootNode.getDispatch() != d && d.getState() == RequestDispatch.State.ESTABLISHED)
@@ -233,7 +233,7 @@ public class Client implements Runnable {
   }
 
   Set<Node> getLostApps() {
-  	return lostApps;
+    return lostApps;
   }
 
 }
